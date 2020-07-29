c4412915024c049cd36956f9cb68937aca08e0e4
Swift.Sun <sunjiwei@yeah.net>
2020-07-29 15:02:26 +0800

    Create quick_sort.cpp

---
+++ quick_sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int paratition(int left, int right, vector<int>& arr)
+{
+    int base = arr[left];
+    int i = left;
+    int j = right;
+    while (i < j) {
+        while (base <= arr[j] && i < j) {
+            j--;
+        }
+        while (base >= arr[i] && i < j) {
+            i++;
+        }
+        swap(arr[i], arr[j]);
+    }
+    swap(arr[left], arr[i]);
+    return i;
+}
+void quickSort(int left, int right, vector<int>& arr)
+{
+    if (left >= right) {
+        return;
+    }
+    int pos = paratition(left, right, arr);
+    quickSort(left, pos - 1, arr);
+    quickSort(pos + 1, right, arr);
+}
+int main()
+{
+    vector<int> arr { 5, 6, 8, 3, 2, 7, 1, 9 };
+    quickSort(0, arr.size(), arr);
+    for (int i : arr) {
+        cout << i << endl;
+    }
+    return 0;
+}
