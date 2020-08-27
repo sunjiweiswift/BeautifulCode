7c7c8d72e10ce026eadff4b290afd9110b39c496
Swoft.Sun <sunjiwei@yeah.net>
2020-08-27 15:18:05 +0800

    add thradpool

--- DataStructure/ThreadTool/thread_pool.h
+++ DataStructure/ThreadTool/thread_pool.h
@@ -1,58 +1,95 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
-#include "blocking_queue.h"
-#include <atomic>
+#include <condition_variable>
 #include <functional>
+#include <future>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <stdexcept>
 #include <thread>
 #include <vector>
 
-#define THREAD_POOL_MAX_TASKS 2
-
 class ThreadPool {
-    using TaskFunc = std::function<void()>;
-    using Task = std::pair<uint32_t, TaskFunc>;
-
 public:
-    ThreadPool(size_t threadNum = 1)
-        : threadNum_(threadNum)
-        , activeNum_(0)
-        , stop_(false)
-    {
-    }
-    ~ThreadPool()
-    {
-    }
-    bool Init()
-    {
-        for (size_t i = 1; i < threadNum_; i++) {
-            size_t threadIndex = i;
-            workers_.emplace([this]() {
-                int  a =1;
-                //     while (!stop_) {
-
-                //         // while (activeNum_ > 0) {
-                //         //     for (size_t j = 0; j < THREAD_POOL_MAX_TASKS; j++) {
-                //         //         if (*tasks_[i].second[threadIndex]) {
-                //         //             tasks_[i].first.first(threadIndex);
-                //         //             {
-                //         //                 *tasks_[i].second[threadIndex] = false;
-                //         //             }
-                //         //         }
-                //         //     }
-                //         // }
-                //     }
-            });
-        }
-    }
+    ThreadPool(size_t);
+    template <class F, class... Args>
+    auto enqueue(F&& f, Args&&... args)
+        -> std::future<typename std::result_of<F(Args...)>::type>;
+    ~ThreadPool();
 
 private:
-    size_t threadNum_;
-    std::atomic_int activeNum_ = { 0 };
-    std::atomic<bool> stop_;
-    // std::vector<std::pair<TASK, std::vector<std::atomic_bool*>>> tasks_;
-    BlockingQueue<Task> taskQueue_;
+    // need to keep track of threads so we can join them
     std::vector<std::thread> workers_;
+    // the task queue
+    std::queue<std::function<void()>> task_;
+
+    // synchronization
+    std::mutex queueMutex_;
+    std::condition_variable condition_;
+    bool stop_;
 };
+
+// the constructor just launches some amount of workers_
+inline ThreadPool::ThreadPool(size_t threads)
+    : stop_(false)
+{
+    for (size_t i = 0; i < threads; ++i)
+        workers_.emplace_back(
+            [this] {
+                for (;;) {
+                    std::function<void()> task;
+
+                    {
+                        std::unique_lock<std::mutex> lock(this->queueMutex_);
+                        this->condition_.wait(lock,
+                            [this] { return this->stop_ || !this->task_.empty(); });
+                        if (this->stop_ && this->task_.empty())
+                            return;
+                        task = std::move(this->task_.front());
+                        this->task_.pop();
+                    }
+
+                    task();
+                }
+            });
+}
+
+// add new work item to the pool
+template <class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args)
+    -> std::future<typename std::result_of<F(Args...)>::type>
+{
+    using return_type = typename std::result_of<F(Args...)>::type;
+
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+    std::future<return_type> res = task->get_future();
+    {
+        std::unique_lock<std::mutex> lock(queueMutex_);
+
+        // don't allow enqueueing after stop_ping the pool
+        if (stop_)
+            throw std::runtime_error("enqueue on stop_ped ThreadPool");
+
+        task_.emplace([task]() { (*task)(); });
+    }
+    condition_.notify_one();
+    return res;
+}
+
+// the destructor joins all threads
+inline ThreadPool::~ThreadPool()
+{
+    {
+        std::unique_lock<std::mutex> lock(queueMutex_);
+        stop_ = true;
+    }
+    condition_.notify_all();
+    for (std::thread& worker : workers_)
+        worker.join();
+}
 
 #endif
