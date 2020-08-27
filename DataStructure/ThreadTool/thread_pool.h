dc2cc359065b5d20a9debdc2d7b56264589a094b
Swoft.Sun <sunjiwei@yeah.net>
2020-08-27 11:10:38 +0800

    delete a.out

--- DataStructure/ThreadTool/thread_pool.h
+++ DataStructure/ThreadTool/thread_pool.h
@@ -10,10 +10,10 @@
 #define THREAD_POOL_MAX_TASKS 2
 
 class ThreadPool {
-public:
     using TaskFunc = std::function<void()>;
     using Task = std::pair<uint32_t, TaskFunc>;
 
+public:
     ThreadPool(size_t threadNum = 1)
         : threadNum_(threadNum)
         , activeNum_(0)
@@ -27,19 +27,21 @@ public:
     {
         for (size_t i = 1; i < threadNum_; i++) {
             size_t threadIndex = i;
-            workers_.emplace([this, threadIndex]() {
-                while (!stop_) {
-                    while (activeNum_ > 0) {
-                        for (size_t j = 0; j < THREAD_POOL_MAX_TASKS; j++) {
-                            if (*tasks_[i].second[threadIndex]) {
-                                tasks_[i].first.first(threadIndex);
-                                {
-                                    *tasks_[i].second[threadIndex] = false;
-                                }
-                            }
-                        }
-                    }
-                }
+            workers_.emplace([this]() {
+                int  a =1;
+                //     while (!stop_) {
+
+                //         // while (activeNum_ > 0) {
+                //         //     for (size_t j = 0; j < THREAD_POOL_MAX_TASKS; j++) {
+                //         //         if (*tasks_[i].second[threadIndex]) {
+                //         //             tasks_[i].first.first(threadIndex);
+                //         //             {
+                //         //                 *tasks_[i].second[threadIndex] = false;
+                //         //             }
+                //         //         }
+                //         //     }
+                //         // }
+                //     }
             });
         }
     }
@@ -48,7 +50,7 @@ private:
     size_t threadNum_;
     std::atomic_int activeNum_ = { 0 };
     std::atomic<bool> stop_;
-    std::vector<std::pair<TASK, std::vector<std::atomic_bool*>>> tasks_;
+    // std::vector<std::pair<TASK, std::vector<std::atomic_bool*>>> tasks_;
     BlockingQueue<Task> taskQueue_;
     std::vector<std::thread> workers_;
 };
