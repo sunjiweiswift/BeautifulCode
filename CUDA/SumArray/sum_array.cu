e92407d3c2b374ad2c9391dd27dc235e422e29a8
Swift.Sun <sunjiwei@yeah.net>
2021-04-13 21:51:05 +0800

    Create sum_array.cu

---
+++ CUDA/SumArray/sum_array.cu
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+__global__ void sum(float* input, float* result, int inputSize) {
+    int index = threadIdx.x;
+    __shared__ float sharedData[16];
+    sharedData[index] = input[index];
+    for (int i = inputSize / 2; i > 0; i /= 2) {
+        if (index < i) {
+            sharedData[index] = sharedData[index] + sharedData[index + i];
+        }
+        __syncthreads();
+    }
+    if (index == 0) {
+        *result = sharedData[0];
+    }
+}
+
+int main(void) {
+    int inputSize = 16;
+    float* inputCPU = new float[inputSize];
+    for (int i = 0; i < 16; i++) {
+        inputCPU[i] = i * (i + 1);
+    }
+    float* inputGPU;
+    float* outputGPU;
+    cudaMalloc(&inputGPU, inputSize * sizeof(float));
+    cudaMalloc(&outputGPU, sizeof(float));
+    cudaMemcpy(inputGPU, inputCPU, inputSize * sizeof(float), cudaMemcpyHostToDevice);
+    sum<<<1, inputSize>>>(inputGPU, outputGPU, inputSize);
+    float outputCPU;
+    cudaMemcpy(&outputCPU, outputGPU, sizeof(float), cudaMemcpyDeviceToHost);
+    printf("result: %f \n", outputCPU);
+    return 0;
+}
