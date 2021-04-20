0155a003cab471e4d36ae9ff98673e818b2d2d40
Swift.Sun <sunjiwei@yeah.net>
2021-04-20 15:42:11 +0800

    midify file dir

--- NN/Img2Col/img2col.cpp
+++
@@ -1,7 +0,0 @@
-void Img2Col(float* img, int channels, int height, int width, int kernelSize, int stride, int pad,
-             float* col) {
-    int colHeight = (height + 2 * pad - kernelSize) / stride + 1;
-    int colWidth = (width + 2 * pad - kernelSize) / stride + 1;
-    int colChannels = channels * kernelSize * kernelSize;
-    
-}
