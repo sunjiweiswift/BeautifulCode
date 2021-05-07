c8b458d1586e29d37a5aea3b6df0e3c073802a05
Swift.Sun <sunjiwei@yeah.net>
2021-05-07 14:57:15 +0800

    add shell sort

--- Sort/InsertSort/shell_sort.cpp
+++ Sort/InsertSort/shell_sort.cpp
@@ -2,7 +2,22 @@
 #include <vector>
 
 using namespace std;
-void ShellSort(vector<int>& arr) {}
+void ShellSort(vector<int>& arr) {
+    int gap = arr.size() / 2;
+    while (gap >= 1) {
+        for (int i = gap; i < arr.size(); i++) {
+            int insertNum = arr[i];
+            int j = i - gap;
+            while (j >= 0 && insertNum < arr[j]) {
+                arr[j + gap] = arr[j];
+                j = j - gap;
+            }
+            arr[j + gap] = insertNum;
+        }
+
+        gap = gap / 2;
+    }
+}
 
 int main() {
     vector<int> arr{5, 6, 8, 3, 2, 7, 1, 9};
