bfc7d7acfc573160a1d181d9336044006e36dd92
Swift.Sun <sunjiwei@yeah.net>
2022-03-16 17:37:55 +0800

    Update top_k_largest.cpp

--- Algorithmn/TopK/top_k_largest.cpp
+++ Algorithmn/TopK/top_k_largest.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    int quickSelect(vector<int>& a, int l, int r, int index) {
-        int q = randomPartition(a, l, r);
-        if (q == index) {
-            return a[q];
-        } else {
-            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);
-        }
-    }
-
-    inline int randomPartition(vector<int>& a, int l, int r) {
-        int i = rand() % (r - l + 1) + l;
-        swap(a[i], a[r]);
-        return partition(a, l, r);
-    }
-
-    inline int partition(vector<int>& a, int l, int r) {
-        int x = a[r], i = l - 1;
-        for (int j = l; j < r; ++j) {
-            if (a[j] <= x) {
-                swap(a[++i], a[j]);
-            }
-        }
-        swap(a[i + 1], a[r]);
-        return i + 1;
-    }
-
     int findKthLargest(vector<int>& nums, int k) {
-        srand(time(0));
-        return quickSelect(nums, 0, nums.size() - 1, nums.size() - k);
+        int left = 0;
+        int right = nums.size() - 1;
+        int pos = paratiton(left, right, nums);
+        k--;
+        while (k != pos) {
+            if (k > pos) {
+                left = pos + 1;
+            } else {
+                right = pos - 1;
+            }
+            pos = paratiton(left, right, nums);
+        }
+        return nums[pos];
+    }
+
+private:
+    int paratiton(int left, int right, vector<int>& nums) {
+        int base = nums[left];
+        int i = left;
+        int j = right;
+        while (i < j) {
+            while (base >= nums[j] && i < j) {
+                j--;
+            }
+            while (base <= nums[i] && i < j) {
+                i++;
+            }
+            swap(nums[i], nums[j]);
+        }
+        swap(nums[left], nums[i]);
+        return i;
     }
 };
